4322e7b2623cea1f189cf1c99eeb533a9a7a4601
Eric Pinto <eric.pinto@hotmail.fr>
2021-06-30 01:42:29 +0200

    finished card_show

--- pokemon_card_generator/frontend/card.py
+++ pokemon_card_generator/frontend/card.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template
+from pathlib import Path
 import os
 
+base_path = os.getcwd()
+set_path = "static/image"
 image_name = "SM135_hires"
-img_path = "../../raw_data/images/"
-set_path = "test"
-set_full_path = img_path + set_path
 image_full_name = image_name + ".png"
-full_path = os.path.join(img_path, set_path, image_full_name)
+
+full_path = os.path.join(set_path, image_full_name)
 
 
-PEOPLE_FOLDER = os.path.join("static", "image")
+base_path2 = Path(os.getcwd()).parents[1]
+set_path2 = "/raw_data/images/test/"
+image_name2 = "SM135_hires"
+image_full_name2 = image_name2 + ".png"
+
+full_path2 = os.path.join(set_path2, image_full_name2)
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = PEOPLE_FOLDER
 
 
 @app.route("/")
 @app.route("/index")
 def show_index():
-    full_filename = os.path.join(app.config["UPLOAD_FOLDER"], "SM135_hires.png")
-    return render_template("index.html", background_image=full_filename)
+    return render_template("index.html", background_image=full_path2)
