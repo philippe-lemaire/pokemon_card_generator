17ea59195e366e64a5b70bc9c728b49a8364a7de
Eric Pinto <eric.pinto@hotmail.fr>
2021-07-01 18:10:32 +0200

    adding labels in card

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with st.form(key="my_form"):
         "<p style='text-align: center; color: black;'>Pokémon name</p>",
         unsafe_allow_html=True,
     )
-    pokemon_name = st.selectbox("", (pokemon_list))
+    pokemon_selected = st.selectbox("", (pokemon_list))
     rarity = ["common", "uncommon", "promo", "rare", "shiny"]
     rarity_selected = st.radio("Rarity", rarity)
     submit_button = st.form_submit_button(label="Generate")
@@ -28,7 +28,6 @@ st.write(
     "<style>div.row-widget.stRadio > div{flex-direction:row;}</style>",
     unsafe_allow_html=True,
 )
-
 
 if rarity_selected == "common":
     rarity_selected = 1
@@ -42,15 +41,28 @@ if rarity_selected == "shiny":
     rarity_selected = 5
 
 # address of the api prediction
-url = "https://api.pokemontcg.io/v2/cards/xy7-19"
+url = "https://api.pokemontcg.io/v2/cards/swsh4-57"
 
-params = dict(name=pokemon_name, rarity=rarity_selected)
+params = dict(name=pokemon_selected, rarity=rarity_selected)
 
 response = requests.get(url, params=params)
 
 prediction = response.json()
 
 pokemon_type = prediction["data"]["types"][0].lower()
+pokemon_name = prediction["data"]["name"]
+hp = prediction["data"]["hp"]
+nationalPokedexNumbers = prediction["data"]["nationalPokedexNumbers"][0]
+
+if prediction["data"].get("abilities"):
+    ability_name = prediction["data"]["abilities"][0]["name"]
+    ability_text = prediction["data"]["abilities"][0]["text"]
+
+# first text
+first_position = 575
+second_position = 625
+third_position = 675
+fourth_position = 725
 
 # paths
 set_path = "pokemon_card_generator/frontend/images/cleaned_cards"
@@ -59,10 +71,32 @@ image_full_path = os.path.join(set_path, pokemon_type + ".png")
 
 def show_image():
     fig = plt.figure(frameon=True, figsize=(9, 12), dpi=100.0)
-    plt.axis("off")
+    plt.axis("on")
 
     ax1 = plt.imshow(mpimg.imread(image_full_path), alpha=0.7)
-    plt.text(130, 70, pokemon_name, fontsize=15, color="black")
+    plt.text(130, 72, pokemon_name, fontsize=25, color="black")
+    plt.text(530, 72, "HP", fontsize=13, color="black")
+    plt.text(560, 72, hp, fontsize=25, color="black")
+    plt.text(
+        367,
+        506,
+        "NO. " + str(nationalPokedexNumbers) + " Pokémon",
+        ha="center",
+        fontsize=12,
+        color="black",
+    )
+    if prediction["data"].get("abilities"):
+        plt.text(200, first_position, ability_name, fontsize=22, color="black")
+        plt.text(
+            50,
+            second_position,
+            ability_text,
+            ha="left",
+            fontsize=16,
+            color="black",
+            wrap=True,
+            transform=plt.transAxes,
+        )
     plt.savefig("pokemon_card_generator/frontend/images/cache/cache_image.png")
     init_image = Image.open(
         "pokemon_card_generator/frontend/images/cache/cache_image.png"
