e044a910c5f728abf3f645d29f39f56d916533a8
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-07-05 14:46:06 +0200

    made flavor text gen more random by picking a real card's flavor text as beginning of prompt

--- pokemon_card_generator/models/flavor_text.py
+++ pokemon_card_generator/models/flavor_text.py
@@ -2,9 +2,31 @@ from pokemon_card_generator import utils
 from pokemon_card_generator.data.scrap_data import get_lore
 import openai
 from os import path
+from random import choice
 
 
-def generate_flavor(pokémon_name, *args, **kwargs):
+def get_prompt(pokémon_name, cards_df):
+    """returns a random beginning of prompt from a random flavor text in cards_df set to this pokémon"""
+    # filter on cards with this name
+    mask = cards_df.name == pokémon_name
+    pokémon_df = cards_df[mask]
+
+    # list the flavor texts
+
+    flavor_texts = [text for text in pokémon_df.flavorText if isinstance(text, str)]
+
+    # remove duplicates
+    flavor_texts = list(set(flavor_texts))
+    # make a random selection
+    selected_flavor = choice(flavor_texts)
+    # cut down the prompt take the 8 first words
+    prompt_as_list = selected_flavor.split(" ")[:6]
+    # slice it up and join it back
+    prompt = " ".join(prompt_as_list)
+    return prompt
+
+
+def generate_flavor(pokémon_name, cards_df, *args, **kwargs):
     """Baseline: uses openAI to generate text"""
 
     # locate the path to the api-file and load it
@@ -14,8 +36,8 @@ def generate_flavor(pokémon_name, *args, **kwargs):
     openai.api_key = utils.get_file_contents(api_file)
 
     # read from the scrapped wiki biology section and pick the first 50 characters
-    lore = get_lore()
-    prompt = lore[pokémon_name][1:50]
+    # lore = get_lore()
+    prompt = get_prompt(pokémon_name, cards_df)
 
     # call the API
     response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=25)
